6c4d4fbfcae723478a961c3be6c70c2b340ba32e
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-05-10 20:37:11 -0400

    add fetch to script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("script js works")
+
+const promises = [];
+for (let i = 1; i <= 151; i++) {
+    promises.push(fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    .then(response => response.json()));
+}
+Promise.all(promises).then(results => {
+    console.log(results);
+});

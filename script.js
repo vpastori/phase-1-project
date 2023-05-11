fd66d95718974c56ea242d22ba2b95c3fd33ebd1
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-05-10 21:34:18 -0400

    add map to script

--- script.js
+++ script.js
@@ -6,5 +6,16 @@ for (let i = 1; i <= 151; i++) {
     .then(response => response.json()));
 }
 Promise.all(promises).then(results => {
-    console.log(results);
+    const pokemon = results.map(data => ({
+        name: data.name,
+        id: data.id,
+        image: data.sprites["front_default"],
+        shinyImage: data.sprites["front_shiny"],
+        type: data.types.map(type => type.type.name).join(", "),
+    }));
+    displayPokemon(pokemon);
 });
+
+const displayPokemon = pokemon => {
+    console.log(pokemon)
+};
